30aa36c1015c7885f03020e374ee4a23d4dcf8bd
natalija <nzaric@vub.hr>
2023-10-19 09:40:21 +0200

    init

---
+++ Pages/Check.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualBasic;
+
+namespace PasswordGen.Pages;
+
+public class CheckModel : PageModel
+{
+    public string PasswordQuality {get; set;}
+    private readonly ILogger<CheckModel> _logger;
+
+    public CheckModel(ILogger<CheckModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        PasswordQuality = "";
+    }
+}
+
---
+++ Pages/Generate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PasswordGen.Pages;
+
+public class GenerateModel : PageModel
+{
+    private readonly ILogger<GenerateModel> _logger;
+
+    public GenerateModel(ILogger<GenerateModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
